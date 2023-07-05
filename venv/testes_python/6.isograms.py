85144f4c2610f86815f5527f5462edb26e21b291
João Campos <joaoagrelacampos@hotmail.com>
2023-07-05 18:11:05 +0100

    --

---
+++ venv/testes_python/6.isograms.py
@@ -0,0 +1,16 @@
+def is_isogram(string):
+    string = string.lower()
+    for letter in string:
+        counting = string.count(letter)
+        print(letter, counting)
+        if counting > 1:
+            return False
+    return True
+
+print(is_isogram('Dermatoglyphics'))
+print(is_isogram('moose'))
+print(is_isogram('aba'))
+print(is_isogram(''))
+
+def is_isogram(string):
+    return len(string) == len(set(string.lower()))
